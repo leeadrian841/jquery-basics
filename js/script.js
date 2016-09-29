56a24c10bff83f597592eb2a9f5bc942c4ad9ef6
leeadrian841 <leeadrian841@gmail.com>
2016-09-29 16:13:58 +0800

    updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
-var input = 5
-var num = input + 1
-var count = "ARGH!"
+// var num = 10
+// var count = "ARGH!"
 var $body = $('body')
-var $ul = $('<ul>')
-$body.append($ul)
-// $ul.text("Count down")
+// var $ul = $('<ul>')
+// $body.append($ul)
+// // $ul.text("Count down")
+//
+// for (var i = 1; i < num; i++) {
+//   var $li = $('<li>')
+//   $li.text(i)
+//   $ul.append($li)
+// }
+//
+// var $allList = $('li')
+// setInterval(function () {
+//   $allList.eq(num - 1).fadeOut('slow')
+//   num -= 1
+// },500)
+$(document).ready(function () {
+  var $breakList = $('#breakfast-foods')
+  var $lunchList = $('#lunch-foods')
 
-for (var i = 1; i < num; i++) {
-  var $li = $('<li>')
-  $li.text(i)
-  $ul.append($li)
-}
+  var $brekkies = $('.brekkie')
+  var $lunchies = $('.lunchie')
+
+  $('#brunch').on('click', moveit)
+  var num = 0
+
+  function moveit() {
+    $brekkies.eq(num).attr('class', 'lunchie')
+    $lunchList.prepend($brekkies.eq(num))
+    num++
+  }
+})
